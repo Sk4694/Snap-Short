4b2971fc109a170f3708724ece5e50440cccdcaf
Sumit Kumar <126609856+Sk4694@users.noreply.github.com>
2024-03-15 00:37:27 +0530

    Add files via upload

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+import "./rightbar.scss";
+import User from "../../assets/images/login3.jpg";
+
+const RightBar = () => {
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggetions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <span>Muskan</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <span>Tripti</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <p>
+                <span>Chanda</span>
+                Shared your post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <p>
+                <span>Radhika</span>
+                Shared your post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Isha</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Rupert</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Suhuuu</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Perce</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Sumyya</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>abhishek</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>abha</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>manishaRani</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>kat</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>asmita</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Tijana Awotwi</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>Sandip</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>niga</span>
+            </div>
+              <span>Online</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://i.postimg.cc/TY6mPs7J/login3.jpg" alt="" />
+              <div className="dot"></div>
+              <span>jannat</span>
+            </div>
+              <span>Online</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
---
+++ src/data/storyData.jsx
@@ -0,0 +1,24 @@
+const storyData = [
+    {
+      id: 1,
+      name: "Surbhi",
+      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      name: "Sumit Kumar",
+      img: "https://images.pexels.com/photos/13916255/pexels-photo-13916255.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      name: "khushi",
+      img: "https://images.pexels.com/photos/13916256/pexels-photo-13916256.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      name: "Ravi",
+      img: "https://images.pexels.com/photos/13916257/pexels-photo-13916257.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+  ];
+
+  export default storyData;
